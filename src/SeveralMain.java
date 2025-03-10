2160f4612e419179b026dde6ac6557a5b0b9ac80
Uday Pratap Singh <upsaug1989@gmail.com>
2025-03-10 08:27:26 +0530

    Multiple Main pushed with error "main is already defined."

---
+++ src/SeveralMain.java
@@ -0,0 +1,17 @@
+
+
+public class SeveralMain {
+
+    public static void main(String [] args)
+    {
+        System.out.println("HI");
+        System.out.println("This is 1st main");
+        System.out.println("End of 1st Main");
+    }
+//    public static void main(String [] args) {
+//    }
+//
+//    public static void main(String args[]){}
+////
+
+}
