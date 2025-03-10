b4c6e869b9764ba55b877d4a94f1494d3f537eeb
Uday Pratap Singh <upsaug1989@gmail.com>
2025-03-10 08:43:24 +0530

    Multiple Main pushed with error "main is already defined."

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hi is Git test repo");
+
+        System.out.println("2nd line to print");
     }
 }
