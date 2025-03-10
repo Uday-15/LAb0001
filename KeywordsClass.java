0886f0eb3f9ef0d2e7cf173ed06d70262e297882
Uday Pratap Singh <upsaug1989@gmail.com>
2025-03-10 09:00:33 +0530

    Identifiers is pushed

---
+++ KeywordsClass.java
@@ -0,0 +1,7 @@
+public class KeywordsClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Keywords are the registered words who has there work already pre-defined in the Java.");
+    }
+}
