af3340e9b28adf7340518e8b31446488b2e34a8e
Uday Pratap Singh <upsaug1989@gmail.com>
2025-03-10 08:58:32 +0530

    Identifiers is pushed

---
+++ src/Identifiers.java
@@ -0,0 +1,8 @@
+public class Identifiers {
+
+//    This java class is used to understand identifiers
+public static void main(String[] args) {
+    System.out.println("Identifiers are the name used to identify certain elements in the program");
+}
+
+}
